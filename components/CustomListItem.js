76cf85ba05bc196684e6bbd72ddbac7ef36549af
KlassiC-Man <parth.11.gulati@gmail.com>
2021-06-11 12:34:42 +0530

    The chat system is now complete

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, StyleSheet, Text} from 'react-native';
 import {ListItem, Avatar} from 'react-native-elements';
+import {db, auth} from '../firebase';
 
 const CustomListItem = ({ id, chatName, enterChat }) => {
+
+  const [chatMessages, setChatMessages] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = db.collection('chats').doc(id).collection('messages').orderBy('timestamp', 'desc').onSnapshot(snapshot => (
+      setChatMessages(snapshot.docs.map(doc => doc.data()))
+    ));
+    return unsubscribe;
+  });
 
   function tell() {
     alert('Thats a chat!')
@@ -10,13 +20,13 @@ const CustomListItem = ({ id, chatName, enterChat }) => {
 
   return(
     <ListItem key={id} bottomDivider onPress={() => enterChat(id, chatName)} onLongPress={tell} containerStyle={{backgroundColor: '#282828'}}>
-      <Avatar rounded source={{uri: 'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png'}} />
+      <Avatar rounded source={{uri: chatMessages[0]?.photoURL || 'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png'}} />
       <ListItem.Content>
         <ListItem.Title style={{fontWeight: "800", color: 'white'}}>
           {chatName}
         </ListItem.Title>
         <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail" style={{color: 'grey'}}>
-          This is a test!
+          {chatMessages[0]?.displayName}: {chatMessages[0]?.message}
         </ListItem.Subtitle>
       </ListItem.Content>
     </ListItem>
--- screens/Chat.js
+++ screens/Chat.js
@@ -10,6 +10,7 @@ import {db, auth} from '../firebase';
 function Chat({navigation, route}) {
 
   const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -18,7 +19,7 @@ function Chat({navigation, route}) {
         headerBackTitle: 'Chats',
         headerTitle: () => (
           <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Avatar rounded source={{uri: 'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png'}} />
+            <Avatar rounded source={{uri: messages[0]?.data.photoURL}} />
             <Text style={styles.headingText}>{route.params.chatName}</Text>
           </View>
         ),
@@ -28,28 +29,53 @@ function Chat({navigation, route}) {
           </TouchableOpacity>
         )
     })
-  }, [navigation]);
+  }, [navigation, messages]);
+
+  useLayoutEffect(() => {
+    const unsubscribe = db.collection('chats').doc(route.params.id).collection('messages').orderBy('timestamp', 'desc').onSnapshot(snapshot => setMessages(
+      snapshot.docs.map(doc => ({
+        id: doc.id,
+        data: doc.data()
+      }))
+    ))
+
+    return unsubscribe;
+  }, [route]);
 
   const sendMsg = () => {
     db.collection('chats').doc(route.params.id).collection('messages').add({
-      //timestamp: firebase.firestore.FieldValue.serverTimeStamp(),
+      timestamp: firebase.firestore.Timestamp.fromDate(new Date()),
       message: input,
       displayName: auth.currentUser.displayName,
       email: auth.currentUser.email,
       photoURL: auth.currentUser.photoURL,
-    })
+    });
+    setInput('');
   };
 
   return(
     <SafeAreaView style={styles.container}>
       <View style={styles.container__main} behavior='height'>
         <>
-          <ScrollView>
-            {/*The messages!*/}
+          <ScrollView contentContainerStyle={{paddingTop:10}}>
+            {messages.map(({id, data}) => (
+              data.email === auth.currentUser.email ? (
+                <View key={id} style={styles.recieved}>
+                  <Avatar source={{uri: data.photoURL}} rounded size={25} position='absolute' bottom={30} right={-5} />
+                  <Text style={styles.recievedMsg}>{data.message}</Text>
+                </View>
+              ):(
+                <View key={id} style={styles.sent}>
+                  <Avatar source={{uri: data.photoURL}} rounded size={25} position='absolute' bottom={60} right={-5} />
+                  <Text style={styles.sentMsg}>{data.message}</Text>
+                  <Text style={styles.sender}>{data.displayName}</Text>
+                </View>
+              )
+            ))}
           </ScrollView>
           <View style={styles.mainArea}>
             <TextInput placeholder='Type The Message' placeholderTextColor='grey' style={styles.textInput} value={input} onChangeText={(text) => setInput(text)} />
-            <TouchableOpacity onPress={sendMsg}>
+            <TouchableOpacity onPress={sendMsg} disabled={!input}>
               <Feather name='send' size={24} color='cadetblue' />
             </TouchableOpacity>
           </View>
@@ -66,6 +92,42 @@ const styles = StyleSheet.create({
     color: 'black',
     marginLeft: 10,
     fontWeight: '700',
+  },
+  recieved: {
+    padding: 15,
+    backgroundColor: 'wheat',
+    alignSelf: "flex-end",
+    borderRadius: 10,
+    marginRight: 15,
+    marginBottom: 20,
+    maxWidth: '80%',
+    position: 'relative',
+  },
+  recievedMsg: {
+    color: 'black',
+    fontWeight: '500',
+    marginLeft: 10,
+  },
+  sent: {
+    padding: 15,
+    backgroundColor: 'cadetblue',
+    alignSelf: 'flex-start',
+    borderRadius: 20,
+    margin: 15,
+    maxWidth: '80%',
+    position: 'relative',
+  },
+  sentMsg: {
+    color: 'white',
+    fontWeight: '500',
+    marginLeft: 10,
+    marginBottom: 15,
+  },
+  sender: {
+    left: 10,
+    paddingRight: 10,
+    fontSize: 10,
+    color: 'wheat',
   },
   container: {
     backgroundColor: '#282828',
